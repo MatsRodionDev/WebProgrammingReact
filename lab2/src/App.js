d4f960fe020f81074c7ec098cfb72ea9285b94de
MatsRodionDev <162904839+MatsRodionDev@users.noreply.github.com>
2024-10-23 12:18:58 +0300

    create app component

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserList from './UserList';
+import AddUser from './AddUser';
+import EditUser from './EditUser';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [editingUser, setEditingUser] = useState(null);
+
+  const addUser = (user) => {
+    setUsers([...users, user]);
+  };
+
+  const deleteUser = (id) => {
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const startEdit = (user) => {
+    setEditingUser(user);
+  };
+
+  const updateUser = (updatedUser) => {
+    setUsers(users.map(user => user.id === updatedUser.id ? updatedUser : user));
+    setEditingUser(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User Cards</h1>
+      <AddUser addUser={addUser} />
+      {editingUser && <EditUser user={editingUser} updateUser={updateUser} />}
+      <UserList users={users} deleteUser={deleteUser} startEdit={startEdit} />
     </div>
   );
-}
+};
 
 export default App;
